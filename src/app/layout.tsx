25000313d5e96dd3b76c2fbc4cebf9ac7c6307c3
gxres042 <i@gxres.net>
2025-01-01 12:20:05 +0800

    chore: style modify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,37 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Online image process",
-  description: "Compress your image and transform its format, then upload it to Cloudflare R2.",
+  description:
+    "Compress your image and transform its format, then upload it to Cloudflare R2.",
 };
+
+function Card({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="bg-white dark:bg-zinc-700 p-8 rounded-lg shadow-md text-center">
+      <hgroup>
+        <h1 className="text-4xl m-0">Online image process</h1>
+        <p>
+          Compress your image and transform its format, then upload it to
+          Cloudflare R2.
+        </p>
+      </hgroup>
+      <br />
+      <div>{children}</div>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="h-6">
+      <p className="text-center">© Restent Ou</p>
+    </footer>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -13,10 +42,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-      className="m-0 bg-sky-100 dark:bg-sky-500/15 max-w-4xl mx-auto"
-      >
-        {children}
+      <body className="m-0 bg-sky-100 dark:bg-sky-500/15 max-w-4xl mx-auto">
+        <div className="min-h-svh p-4 flex flex-col items-center justify-center">
+          <div className="flex-1 flex items-center justify-center">
+            <Card>{children}</Card>
+          </div>
+          <Footer />
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,9 @@
 import { Form } from "@/components/form";
 
-function Card() {
+export default function IndexPage() {
   return (
-    <div className="bg-white dark:bg-zinc-700 p-8 rounded-lg shadow-md text-center">
-      <hgroup>
-        <h1 className="text-4xl m-0">Online image process</h1>
-        <p>
-          Compress your image and transform its format, then upload it to
-          Cloudflare R2.
-        </p>
-      </hgroup>
-      <br />
+    <>
       <Form />
-    </div>
-  );
-}
-
-function Footer() {
-  return (
-    <footer className="h-6">
-      <p className="text-center">© Restent Ou</p>
-    </footer>
-  );
-}
-
-export default function Home() {
-  return (
-    <div className="min-h-svh p-4 flex flex-col items-center justify-center">
-      <div className="flex-1 flex items-center justify-center">
-        <Card />
-      </div>
-      <Footer />
-    </div>
+    </>
   );
 }
